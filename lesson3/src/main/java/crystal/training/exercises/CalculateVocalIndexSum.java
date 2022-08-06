86f627d2214da1f816b294c0384e0e9ac151a69f
Indrit Vaka <indrit.vaka@crystal-system.ue>
2022-08-06 18:07:38 +0200

    Completing exercises of lesson 3

---
+++ lesson3/src/main/java/crystal/training/exercises/CalculateVocalIndexSum.java
@@ -0,0 +1,17 @@
+package crystal.training.exercises;
+
+import java.util.stream.IntStream;
+
+public class CalculateVocalIndexSum
+{
+    static final String VOCALS = "aeiou";
+    static String myText = "There also could occur errors during search or poison application Or the search could take a very long time because the ants hide very well";
+
+    public static void main(String[] args)
+    {
+
+        int sum = IntStream.range(0, myText.length()).filter(v -> VOCALS.contains(myText.charAt(v) + "")).sum();
+        System.out.println(sum);
+    }
+
+}
---
+++ lesson3/src/main/java/crystal/training/exercises/StreamOfChar.java
@@ -0,0 +1,14 @@
+package crystal.training.exercises;
+
+import java.util.stream.Stream;
+
+public class StreamOfChar {
+    static final String VOCALS = "aeiou";
+
+    static String myText = "There also could occur errors during search or poison application Or the search could take a very long time because the ants hide very well";
+
+    // TO DO - find 2 other ways to create a stream of chars from a string !!!!
+    public static void main(String[] args) {
+        Stream<Character> characterStream = new String(myText).chars().mapToObj(i->(char)i);
+    }
+}
