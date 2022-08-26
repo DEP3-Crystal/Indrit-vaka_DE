8fd419fab6f4264f3002f7086506e3e280e72c50
Indrit Vaka <indrit.vaka@crystal-system.eu>
2022-08-26 10:04:06 +0200

    lesson 7 GroupBy

---
+++ lesson7/src/main/java/reduce/model/City.java
@@ -0,0 +1,40 @@
+package reduce.model;
+
+public class City {
+    private String name;
+    private String state;
+    private int people;
+    private double area;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getPeople() {
+        return people;
+    }
+
+    public void setPeople(int people) {
+        this.people = people;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+}
